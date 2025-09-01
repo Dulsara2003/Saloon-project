07a0e9695b59e531b46d30358958e845c53e827e
Dulsara Dissanayaka <dulsaradissanayaka2003@gmail.com>
2025-09-02 00:33:30 +0530

    Zoom animation,Services all done

--- saloon-site/index.php
+++ saloon-site/index.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Web Design</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="all.css">
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="about.css">
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="services.css">
 </head>
 
 <body style="background-color: #0C0C0C;">
@@ -37,10 +37,10 @@
                     <!-- Mobile -->
                     <div class="row justify-content-center d-block d-md-none"> <!-- Center the content -->
                         <div class="col-12 main-text-balance text-center animate-left"> <!-- 10 columns on mobile, 5 columns on larger screens -->
-                            <h1 class="fw-bold header-mobile">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
+                            <h1 class="fw-bold header-mobile fade-right">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
                         </div>
 
-                        <div class="animate-bottom text-center">
+                        <div class="fade-up text-center">
                             <p class="mx-auto text-balance ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             </p>
@@ -52,11 +52,11 @@
                     <!-- Md -->
                     <div class="row d-none d-md-block d-lg-none "> <!-- Center the content -->
                         <div class="col-12 main-text-balance text-center animate-left"> <!-- 10 columns on mobile, 5 columns on larger screens -->
-                            <h1 class="fw-bold header-md">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
+                            <h1 class="fw-bold header-md fade-right">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
 
                         </div>
 
-                        <div class="animate-bottom">
+                        <div class="fade-up">
                             <p class="mx-auto text-balance ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             </p>
@@ -68,10 +68,10 @@
                     <div class="row d-none d-lg-block d-xl-none"> <!-- Center the content -->
                         <div class="col-10 col-md-10 main-text-balance animate-left"> <!-- 10 columns on mobile, 5 columns on larger screens -->
 
-                            <h1 class="fw-bold header-lg">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
+                            <h1 class="fw-bold header-lg fade-right">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
                         </div>
 
-                        <div class="animate-bottom">
+                        <div class="fade-up">
                             <p class="col-9 text-balance ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             </p>
@@ -82,12 +82,12 @@
 
 
                     <!-- Xl -->
-                    <div class="row d-none d-xl-block "> <!-- Center the content -->
+                    <div class="row d-none d-xl-block"> <!-- Center the content -->
                         <div class="col-9 main-text-balance animate-left"> <!-- 10 columns on mobile, 5 columns on larger screens -->
-                            <h1 class="fw-bold header-xl">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
+                            <h1 class="fw-bold header-xl fade-right">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h1>
                         </div>
 
-                        <div class="animate-bottom">
+                        <div class="fade-up">
                             <p class="col-9 text-balance ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                             </p>
@@ -100,12 +100,12 @@
         </div>
 
         <!-- 2nd Content (About) -->
-        <div id="scrollspyHeading2" class="container about-section">
+        <div id="scrollspyHeading2" class="container about-section ">
             <h2 class="text-center about-title mb-5 fade-up">About</h2>
             <div class="row align-items-center">
 
                 <div class="col-11 col-lg-6 mb-5 container">
-                    <p class="about-content text-white text-balance fade-right-about-text" style="font-family: 'Franklin Gothic Medium', Arial, sans-serif;font-size: 19px; ">
+                    <p class="about-content text-white text-balance fade-right" style="font-family: 'Franklin Gothic Medium', Arial, sans-serif;font-size: 19px; ">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute.
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -113,7 +113,7 @@
                 </div>
 
                 <div class="col-lg-6 d-flex  justify-content-center justify-content-lg-end">
-                    <div class="about-box fade-right-about-img ">
+                    <div class="about-box fade-left">
                         <img src="Resources/about.jpg" class="rounded-image">
                     </div>
                 </div>
@@ -124,20 +124,41 @@
 
         <!-- 3rd Content (Services) -->
         <div id="scrollspyHeading3" class="container about-section">
-            <h2 class="text-center about-title mb-5">Services</h2>
+            <h2 class="text-center about-title mb-5 fade-up ">Services</h2>
             <div class="row align-items-center">
 
-                <div class="col-11 col-lg-6 mb-5 container">
-                    <p class="about-content text-white text-balance " style="font-family: 'Franklin Gothic Medium', Arial, sans-serif;font-size: 19px; ">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                </div>
+                <div class="container py-5">
+                    <div class="row text-center justify-content-center mb-4">
+                        <div class="col-12 col-md-4 mb-4">
 
-                <div class="col-lg-6 d-flex  justify-content-center justify-content-lg-end">
-                    <div class="about-box">
-                        <img src="Resources/about.jpg" class="rounded-image">
+                            <div class="gallery-title fade-up ">Gents</div>
+                            <div class="gallery-card fade-left">
+                               
+                            </div>
+
+                        </div>
+
+                        <div class="col-12 col-md-4 mb-4">
+                            <div class="gallery-title fade-up ">Ladies</div>
+                            <div class="gallery-card zoom-in"></div>
+                        </div>
+
+                        <div class="col-12 col-md-4 mb-4">
+                            <div class="gallery-title fade-up ">Kids</div>
+                            <div class="gallery-card fade-right"></div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="row text-center">
+                        <div class="col-12 fade-up ">
+                            <p class="px-3 text-white  ">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            </p>
+                            <button class="our-gallery-btn">Our Gallery</button>
+                        </div>
                     </div>
                 </div>
 
@@ -213,8 +234,10 @@
 
 
     </div>
-
+    <script src="all.js"></script>
+    <script src="home.js"></script>
     <script src="about.js"></script>
+    <script src="services.js"></script>
 </body>
 
 </html>
